63311a1d6ccefb0dc10e4b9df8207d15ccf6a2de
Marek Pfliegler <xpfliegl@fi.muni.cz>
2017-03-08 10:52:29 +0100

    add customer and test

---
+++ pv168project/src/main/java/cz/muni/fi/CustomerManager/CustomerManager.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.CustomerManager;
+
+/**
+ * Created by Marek Pfliegler on 8.3.2017.
+ */
+public class CustomerManager {
+}
---
+++ pv168project/src/test/java/cz/muni/fi/Test.java
@@ -0,0 +1,7 @@
+package cz.muni.fi;
+
+/**
+ * Created by pefa1 on 8.3.2017.
+ */
+public class Test {
+}
