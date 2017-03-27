92cd2386239196bf7282635ee72334260d01edc2
Josef Kosta <433545@mail.muni.cz>
2017-03-27 19:41:27 +0200

    for pull

--- pv168project/src/main/java/cz/muni/fi/RentManagerImpl.java
+++ pv168project/src/main/java/cz/muni/fi/RentManagerImpl.java
@@ -2,8 +2,11 @@ package cz.muni.fi;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -45,25 +48,56 @@ public class RentManagerImpl implements RentManager {
         if (rent.getBook().getId() == null) {
             throw new IllegalArgumentException("book id is null");
         }
+        if(rent.getRentTime() != null){
+            throw new IllegalEntityException("rent time should be null");
+        }
+        if(rent.getReturnTime() != null){
+            throw new IllegalEntityException("return time should be null");
+        }
+        if(rent.getReturnTime() != null){
+            throw new IllegalEntityException("return time should be null");
+        }
+
+        if(rent.getExpectedReturnTime() == null || !LocalDate.now().isBefore(rent.getExpectedReturnTime())){
+            throw new IllegalEntityException("expected return date should be after present");
+        }
+
         if(!checkBookIsAvailabel(rent.getBook().getId())){
             throw new IllegalArgumentException("book is not available");
         }
+
         Connection conn = null;
-        PreparedStatement updateSt = null;
+        PreparedStatement st = null;
         try {
             conn = dataSource.getConnection();
 
             conn.setAutoCommit(false);
 
-            updateSt = conn.prepareStatement(
+            //adding rent to database
+            st = conn.prepareStatement(
+                    "INSERT INTO Rent (rentTime, expectedReturnTime, returnTime) VALUES (?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            st.setDate(1, toSqlDate(LocalDate.now()));
+            st.setDate(2, toSqlDate(rent.getExpectedReturnTime()));
+            st.setDate(3, toSqlDate(rent.getReturnTime()));
+
+
+            int count = st.executeUpdate();
+            DBUtils.checkUpdatesCount(count, book, true);
+
+            Long id = DBUtils.getId(st.getGeneratedKeys());
+            book.setId(id);
+            conn.commit();
+
+            st = conn.prepareStatement(
                     "UPDATE Rent SET bookId = ? WHERE id = ? AND bookId IS NULL");
-            updateSt.setLong(1, rent.getBook().getId());
-            updateSt.setLong(2, rent.getId());
-            updateSt = conn.prepareStatement(
+            st.setLong(1, rent.getBook().getId());
+            st.setLong(2, rent.getId());
+            st = conn.prepareStatement(
                     "UPDATE Rent SET customerId = ? WHERE id = ? AND customerId IS NULL");
-            updateSt.setLong(1, rent.getCustomer().getId());
-            updateSt.setLong(2, rent.getId());
-            int count = updateSt.executeUpdate();
+            st.setLong(1, rent.getCustomer().getId());
+            st.setLong(2, rent.getId());
+            count = st.executeUpdate();
             if (count == 0) {
                 throw new IllegalEntityException(
                         "Rent " + rent + " not found or it has customer and book set");
@@ -76,7 +110,7 @@ public class RentManagerImpl implements RentManager {
             throw new ServiceFailureException(msg, ex);
         } finally {
             DBUtils.doRollbackQuietly(conn);
-            DBUtils.closeQuietly(conn, updateSt);
+            DBUtils.closeQuietly(conn, st);
         }
     }
 
@@ -113,4 +147,13 @@ public class RentManagerImpl implements RentManager {
     private boolean checkBookIsAvailabel(Long bookId){
         return false;
     }
+
+    private static Date toSqlDate(LocalDate localDate) {
+        return localDate == null ? null : Date.valueOf(localDate);
+    }
+
+    private static LocalDate toLocalDate(Date date) {
+        return date == null ? null : date.toLocalDate();
+    }
+
 }
